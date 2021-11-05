aa974e59dc6b043817dcc8cca378a0a6e20d43da
rut <aelob62@gmail.com>
2021-11-05 19:50:21 +0500

    добавил -степень

--- src/lesson5/HomeRecStepNumber.java
+++ src/lesson5/HomeRecStepNumber.java
@@ -2,15 +2,21 @@ package lesson5;
 
 public class HomeRecStepNumber {
     //возведение числа в степень с помощью рекурсии
-    public static int recStepNum(int a, int b) {
-        if (b <= 0) {
+    public static double recStepNum(double a, int b) {
+        if (b == 0) {
             return 1;
         }
-        return recStepNum(a, b - 1) * a;
+        if (b > 0) {
+            return recStepNum(a, b - 1) * a;
+        } else {
+            b = -b;
+            return 1 / (recStepNum(a, b - 1) * a);
+        }
     }
-    public static int recStepNumber(int a, int b) {
-        int c = recStepNum(a, b);
-        System.out.println(a + " в степень " + b + " равно "+ c);
-        return 1;
+
+    public static boolean recStepNumber(double a, int b) {
+        double c = recStepNum(a, b);
+        System.out.println(a + " в степень " + b + " равно " + c);
+        return true;
     }
 }
