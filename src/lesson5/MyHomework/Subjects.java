0b159328c19b99f1c90d7d3168f23c90b2bcf484
rut <aelob62@gmail.com>
2021-11-06 20:15:26 +0500

    lesson5 задача о рюкзаке

---
+++ src/lesson5/MyHomework/Subjects.java
@@ -0,0 +1,12 @@
+package lesson5.MyHomework;
+
+public class Subjects {
+    public int weight;
+    public int price;
+
+    public Subjects(int weight, int price) {
+        this.weight = weight;
+        this.price = price;
+    }
+}
+
